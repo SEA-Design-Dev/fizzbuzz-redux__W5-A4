07c507e1b6eb821239ebee8169c85c792b88c216
Brian Finck <brian@brianfinck.com>
2015-09-30 14:59:25 -0700

    making it more obj

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
 'use strict';
 
 (function(){
-	var arr = [];
-	var string;
-	var valueLrg;
-	var valueSml;
-	
-	var fizzBuzz = {
-		read: function() {
-			valueSml = parseInt(document.getElementById('valueSml').value);
-			valueLrg = parseInt(document.getElementById('valueLrg').value);
-		},
-		calculate: function(sml,lrg) {
-			do {
-				if (sml % 3 === 0 && sml % 5 === 0) {
-					arr.push('FizzBuzz');
-				} else if (sml % 3 === 0) {
-					arr.push('Fizz');
-				} else if (sml % 5 === 0) {
-					arr.push('Buzz');
-				} else {
-					arr.push(sml);
-				} sml ++;
-			} while (sml <= lrg);
-		},
-		write: function() {
-			string = arr.toString();
-			document.getElementById("write").innerHTML = string;
-			arr = [];
+	var FizzBuz = (function(){
+		var arr = [];
+		var string;
+		var valueLrg;
+		var valueSml;
+
+		var fizzBuzz = {
+			read: function() {
+				valueSml = parseInt(document.getElementById('valueSml').value);
+				valueLrg = parseInt(document.getElementById('valueLrg').value);
+			},
+			calculate: function(sml,lrg) {
+				do {
+					if (sml % 3 === 0 && sml % 5 === 0) {
+						arr.push('FizzBuzz');
+					} else if (sml % 3 === 0) {
+						arr.push('Fizz');
+					} else if (sml % 5 === 0) {
+						arr.push('Buzz');
+					} else {
+						arr.push(sml);
+					} sml ++;
+				} while (sml <= lrg);
+			},
+			write: function() {
+				string = arr.toString();
+				document.getElementById("write").innerHTML = string;
+				arr = [];
+			}
 		}
-	};
+		
+		return fizzBuzz;
+	}());
 
 	document.getElementById('submit').addEventListener("click", function(e){
 		e.preventDefault();
