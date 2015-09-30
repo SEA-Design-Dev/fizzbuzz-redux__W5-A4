f3d653ff39875e172c1cf2a9ba54de6f10b2c6fd
Brian Finck <brian@brianfinck.com>
2015-09-30 12:01:38 -0700

    summit

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-var fizzBuzz = function() {
-	
-	var arr = [];
-	var numb = 1;
+//(function(){
+	var fizzBuzz = { 
+		read: function(sml,lrg) {
+			var arr = [];
+			var numb = sml;
+			var outPut;
 
-	do {
-		if (numb % 3 === 0 && numb % 5 === 0) {
-			'FizzBuzz'.push(arr);
-		} else if (numb % 3 === 0) {
-			'Fizz'.push(arr);
-		} else if (numb % 5 === 0) {
-			'Buzz'.push(arr);
-		} else {
-			numb.push(arr);
-		} numb ++;
-	} while (numb < 100);
-}
+			do {
+				if (numb % 3 === 0 && numb % 5 === 0) {
+					arr.push('FizzBuzz');
+				} else if (numb % 3 === 0) {
+					arr.push('Fizz');
+				} else if (numb % 5 === 0) {
+					arr.push('Buzz');
+				} else {
+					arr.push(numb);
+				} numb ++;
+			} while (numb < lrg);
+			outPut = arr.toString();
+			return outPut
+			console.log(outPut);
+		},
+		write: function() {
+			document.body.innerHTML = fizzBuzz.string.outPut; 
+		}
+	};
 
-function newFizzBuzz() {
-	document.getElementById('button').onclick = new fizzBuzz();
-}
+	document.getElementById('submit').addEventListener("click", function(e){
+
+		e.preventDefault();
+		var valueSml = parseInt(document.getElementById('valueSml').value);
+		var valueLrg = parseInt(document.getElementById('valueLrg').value);
+		fizzBuzz.read.call(undefined, valueSml, valueLrg);
+		fizzBuzz.write.call('undefined');
+
+});
+//}());
