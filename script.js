b022150c5e5170676de4e42c1744b0ef880bb309
Brian Finck <brian@brianfinck.com>
2015-10-01 10:58:12 -0700

    done

--- script.js
+++ script.js
@@ -1,40 +1,6 @@
 'use strict';
 
 (function(){
-	var FizzBuz = (function(){
-
-		var fizzBuzz = function () {};
-		var arr = [];
-		var str;
-		var valueLrg;
-		var valueSml;
-
-		fizzBuzz.prototype = {
-			read: function() {
-				valueSml = parseInt(document.getElementById('valueSml').value);
-				valueLrg = parseInt(document.getElementById('valueLrg').value);
-			},
-			calculate: function() {
-				do {
-					if (valueSml % 3 === 0 && valueSml % 5 === 0) {
-						arr.push('FizzBuzz');
-					} else if (valueSml % 3 === 0) {
-						arr.push('Fizz');
-					} else if (valueSml % 5 === 0) {
-						arr.push('Buzz');
-					} else {
-						arr.push(valueSml);
-					} valueSml ++;
-				} while (valueSml <= valueLrg);
-			},
-			write: function() {
-				str = arr.toString();
-				return str;
-			}
-		};
-		return fizzBuzz;
-	}());
-
 	document.getElementById('submit').addEventListener("click", function(e){
 		e.preventDefault();
 		var fb = new FizzBuz();
