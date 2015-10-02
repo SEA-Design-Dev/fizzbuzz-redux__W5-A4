c8416f2f87fce1e931bbe954bd3f8edd4b02fa97
Zachary Gordon <z.gordon.h@gmail.com>
2015-10-01 22:50:41 -0700

    Add fizzbuzz object

--- js/index.js
+++ js/index.js
@@ -43,8 +43,8 @@ var FizzBuzz = (function () {
 document.getElementById("button-a").addEventListener("click", function (e) {
 var myfizz = new FizzBuzz('bleep', 'blorp');
   e.preventDefault();
-  aVal  = document.getElementById("min").value;
-  zVal  = document.getElementById("max").value;
+  aVal  = 1;
+  zVal  = 10;
   myfizz.read(aVal, zVal);
   myfizz.write(aVal, zVal);
 });
