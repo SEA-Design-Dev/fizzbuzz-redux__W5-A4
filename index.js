57fedab169617956cfc1b097dabf5468d13361d3
Zachary Gordon <z.gordon.h@gmail.com>
2015-10-02 00:15:42 -0700

    Refactor input/output

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+var FBcalculator = require("./js/fizzbuzz");
+
 exports.a4fizzbuzz = function () {
 var myfizz = new FizzBuzz('bleep', 'blorp');
-  e.preventDefault();
   aVal  = 1; //min
   zVal  = 10; //max
   myfizz.input(aVal, zVal);
