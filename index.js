f056f5fab7f31d9507f5797d568c4d5afb819b16
Zachary Gordon <z.gordon.h@gmail.com>
2015-10-02 00:08:02 -0700

    Refactor input/output

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-exports.printMsg = function () {
-  console.log("this is a message");
-}
-
-
-
-
-// document.getElementById("button-a").addEventListener("click", function (e) {
-// var myfizz = new FizzBuzz('bleep', 'blorp');
-//   e.preventDefault();
-//   aVal  = 1; //min
-//   zVal  = 10; //max
-//   myfizz.read(aVal, zVal);
-//   myfizz.write(aVal, zVal);
-// });
+exports.a4fizzbuzz = function () {
+var myfizz = new FizzBuzz('bleep', 'blorp');
+  e.preventDefault();
+  aVal  = 1; //min
+  zVal  = 10; //max
+  myfizz.input(aVal, zVal);
+  myfizz.output(aVal, zVal);
+});
 
 
