4549dc6be0bd59886c9e6449699b00d58130f917
Zachary Gordon <z.gordon.h@gmail.com>
2015-10-01 23:04:34 -0700

    Add fizzbuzz object

--- js/index.js
+++ js/index.js
@@ -1,52 +1,17 @@
-var FizzBuzz = (function () {
-  var aVal, zVal
-
-  var _fizzbuzz = function(newString1, newString2) {
-    this.newString1 = newString1 || "Fuzz";
-    this.newString2 = newString2 || "Bizz";
-  };
-
-  var calculate = function(aVal, zVal, newString1, newString2){
-  var myArray =[];
-    for ( aVal ; aVal <= zVal; aVal ++) {
-      if (aVal % 5 === 0 && aVal % 3 === 0) {
-        myArray.push(newString1+newString2)
-      } else if ( aVal % 3 === 0) {
-        myArray.push(newString1);
-      } else if (aVal % 5 === 0) {
-        myArray.push(newString2);
-      } else {
-        myArray.push(aVal);
-      }
-    }
-    return myArray;
-  };
+exports.printMsg = function () {
+  console.log("this is a message");
+}
 
 
-  _fizzbuzz.prototype = {
-    read: function (aVal, zVal) {
-      this.result = calculate(aVal, zVal, this.newString1, this.newString2);
-      },
-    write: function(){
-      document.getElementById("output").innerHTML="";
-      var newEl = document.createElement("p");
-      var newtext = document.createTextNode(this.result);
-      newEl.appendChild(newtext);
-      document.getElementById("output").appendChild(newEl);
-    }
-  }
 
-  return _fizzbuzz;
 
-}());
-
-document.getElementById("button-a").addEventListener("click", function (e) {
-var myfizz = new FizzBuzz('bleep', 'blorp');
-  e.preventDefault();
-  aVal  = 1;
-  zVal  = 10;
-  myfizz.read(aVal, zVal);
-  myfizz.write(aVal, zVal);
-});
+// document.getElementById("button-a").addEventListener("click", function (e) {
+// var myfizz = new FizzBuzz('bleep', 'blorp');
+//   e.preventDefault();
+//   aVal  = 1; //min
+//   zVal  = 10; //max
+//   myfizz.read(aVal, zVal);
+//   myfizz.write(aVal, zVal);
+// });
 
 
