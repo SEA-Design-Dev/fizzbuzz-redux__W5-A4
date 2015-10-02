aa13db4fc76625d7b4b6bcf0ac3ecea6cfbd9cfd
Peter Strumolo <Peter Strumolo>
2015-10-02 10:12:27 -0700

    Update the package.json file

--- js/test.js
+++
@@ -1,6 +0,0 @@
-var FizzBuzz = require('./index.js');
-
-var buzzer = new FizzBuzz("fizzy", "buzzyzy"); // Should accept string arguments that replace 'Fizz' and 'Buzz'
-buzzer.input(1, 16);
-
-console.log(buzzer.output());
